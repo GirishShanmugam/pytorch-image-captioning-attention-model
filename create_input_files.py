115da1e9f25361e906807c562a6ad554c99d1860
girishshanmugam <s.girishshanmugam@gmail.com>
2018-09-26 11:07:32 +0530

    Initial commit

---
+++ create_input_files.py
@@ -0,0 +1,13 @@
+from utils import create_input_files
+import os
+
+if __name__ == '__main__':
+    # Create input files (along with word map)
+    CURR_DIR = os.getcwd()
+    create_input_files(dataset='atlas',
+                       karpathy_json_path=CURR_DIR +'/datasets/toy_dataset/indian_clothing_with_title_toy.json',
+                       image_folder=CURR_DIR +'/datasets/toy_dataset/',
+                       captions_per_image=1,
+                       min_word_freq=20,
+                       output_folder=CURR_DIR +'/outputs/',
+                       max_len=15)
